178d2bd8db5a1f4030dd3d9fffff4dcb1eb0b240
softgenai <admin@softgen.ai>
2026-06-17 18:35:28 +0000

    fix(ui): resolve duplicate header component usage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
-import { Footer } from "@/components/Footer";
-import { AnimatedBackground } from "@/components/AnimatedBackground";
-import { LiveGovernanceWidget } from "@/components/LiveGovernanceWidget";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <AnimatedBackground />
-      <LiveGovernanceWidget />
+    <>
       <Component {...pageProps} />
-      <Footer />
       <Toaster />
-    </ThemeProvider>
+    </>
   );
 }
